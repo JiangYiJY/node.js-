a250848c361a400b651d5aae470747f0606217f1
JiangYiJY <2275453509@qq.com>
2018-02-07 21:21:43 +0800

    2018/02/07 second commit

--- Blog/routers/admin.js
+++ Blog/routers/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
+var Category = require("../models/Category")
 
 //先去进行管理员身份的判断
 router.use(function(req, res, next){
@@ -64,11 +65,184 @@ router.get("/user", function(req, res, next){
 	})
 })
 
+/*
+	分类首页
+*/
+router.get("/category", function(req, res){
+	var page = req.query.page || 1;
+	var limit = 2;
+
+	/*
+		【注】在这里一定要注意，我们的页数不能低于1，也不能大于数据库中总页数
+
+	*/
+	Category.count().then(function(count){
+		// console.log(count);
+		//取值限制
+		var pages = Math.ceil(count / limit);
+		//取值不能超过pages
+		page = Math.min(page, pages);
+		//取值不能小于1
+		page = Math.max(page, 1);
+		var skip = (page - 1) * limit;
+
+		Category.find().limit(limit).skip(skip).then(function(categories){
+			// console.log(users); 可以看数据
+			res.render("admin/category_index", {
+				userInfo: req.userInfo,
+				categories: categories,
+				page: Number(page),  //前端页面需要知道当前是第几页。
+				pages: Number(pages)
+			})
+		})
+	})
+})
+
+/*
+	分类添加的
+*/
+router.get("/category/add", function(req, res){
+	res.render("admin/category_add", {
+		userInfo: req.userInfo
+	})
+})
+
+/*
+	分类保存
+
+	根据post提交表单数据，进行分类保存
+	数据库
+	mongoose
+	建立结构 -> 建立模型 -> 引入模型
+*/
+router.post("/category/add", function(req, res){
+	/*
+		处理post提交过来的数据
+		对提交过来的数据进行验证
+	*/
+	// console.log(req.body.name);
+	var name = req.body.name || "";
+	if(name == ""){
+		//如果为空，我们渲染一个错误提示页面
+		res.render("admin/error", {
+			userInfo: req.userInfo,
+			message: "名称不能为空"
+		});
+		return;
+	}
+
+	//验证数据库中是否已经存在同名的分类名称
+	Category.findOne({
+		name: name
+	}).then(function(rs){
+		if(rs){
+			//数据库中已经存在了该分类
+			res.render("admin/error", {
+				userInfo: req.userInfo,
+				message: "分类已经存在了"
+			})
+			return Promise.reject();
+		}else{
+			//数据库中没有改分类
+			return new Category({
+				name: name
+			}).save();
+		}
+	}).then(function(newCategory){
+		res.render("admin/success", {
+			userInfo: req.userInfo,
+			message: "分类保存成功",
+			url: "/admin/category"
+		})
+	})
+})
+
+
+/*
+	分类修改
+*/
+router.get("/category/edit", function(req, res){
+	//获取我要修改的分类的id
+	var id = req.query.id || "";
+	//查找数据库
+	Category.findOne({
+		_id: id
+	}).then(function(category){
+		if(!category){
+			res.render("admin/error", {
+				userInfo: req.userInfo,
+				message: "分类信息不存在"
+			})
+		}else{
+			//跳转到编译预览页面
+			res.render("admin/category_edit", {
+				userInfo: req.userInfo,
+				category: category
+			})
+		}
+	})
+})
+
+/*
+	分类信息的保存
+*/
+router.post("/category/edit", function(req, res){
+	//获取我要修改的分类的id
+	var id = req.query.id || "";
+	var name = req.body.name || "";
+
+	//查找数据库
+	Category.findOne({
+		_id: id
+	}).then(function(category){
+		if(!category){
+			res.render("admin/error", {
+				userInfo: req.userInfo,
+				message: "分类信息不存在"
+			})
+			return Promise.reject();
+		}else{
+			
+			//当用户没有做任何修改提交的时候
+			if(name == category.name){
+				res.render("admin/success", {
+					userInfo: req.userInfo,
+					message: "修改成功",
+					url: "/admin/category"
+				});
+				return Promise.reject();
+			}else{
+				//要修改的分类名称是否在数据库中已经存在了
+				return Category.findOne({
+					name: name,
+					_id: {$ne: id}
+				})
+			}
+		}
+	}).then(function(sameCategory){
+		if(sameCategory){
+			res.render("admin/error", {
+				userInfo: req.userInfo,
+				message: "数据库中已经存在同名的分类了"
+			})
+			return Promise.reject();
+		}else{
+			return Category.update({
+				_id: id
+			}, {
+				name: name
+			})
+		}
+	}).then(function(){
+		res.render("admin/success", {
+			userInfo: req.userInfo,
+			message: "修改成功",
+			url: "/admin/category"
+		})
+	})
+})
+
 module.exports = router;
-
-
-
-
 
 
 
--- Blog/routers/main.js
+++ Blog/routers/main.js
@@ -1,15 +1,20 @@
 var express = require("express");
 var router = express.Router();
+var Category = require("../models/Category");
 
 router.get("/", function(req, res, next){
 	// res.send("MAIN - User");
 
-	console.log(req.userInfo);
+	// console.log(req.userInfo);
 
-	//加载 index.html
-	res.render("main/index", {
-		userInfo: req.userInfo
-	});
+	Category.find().then(function(categories){
+		//加载 index.html
+		res.render("main/index", {
+			userInfo: req.userInfo,
+			categories: categories
+		});
+	})
+
 })
 
 module.exports = router;
